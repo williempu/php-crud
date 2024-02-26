bceffe28c619838384df0997e313ce0f2c356fea
williem <williem@president.ac.id>
2024-02-26 15:00:07 +0700

    [IMP] add style

---
+++ deleteMajor.php
@@ -0,0 +1,16 @@
+<?php
+require_once("header.php");
+require_once("database.php");
+$db = new DBConnection();
+if (isset($_POST["submit"]) and isset($_GET["id"])) {
+    $db->deleteMajor($_GET["id"]);
+    header("Location: listMajor.php");
+}
+?>
+<div class="container">
+    <form action="" method="post">
+        Are you sure you want to delete this data?<br/>
+        <input class="mt-3 btn btn-danger" type="submit" name="submit" value="Confirm"/>
+        <a class="mt-3 btn btn-info" href="listMajor.php">Cancel</a>
+    </form>
+</div>
